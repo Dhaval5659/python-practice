790de4797d8be1046e1c2996b7405b7b772cee84
Dhaval <dhavalmangukiya.work@gmail.com>
2026-07-28 16:49:03 +0530

    add ifelse.py and match.py for practicing conditional statements and pattern matching

---
+++ 28-07/ifelse/ifelse.py
@@ -0,0 +1,19 @@
+x = 5
+y = 5
+if x>y :
+ print("x is greater than y")
+elif x==y:
+ print("x is equal to y")
+else:
+ print("y id greater than x")
+
+if 4<3: print("4 is greater than 3")
+
+print("5 is less than 6") if 5<6 else print(0)
+
+age = 16
+
+if age < 18:
+  pass 
+else:
+  print("Access granted")
