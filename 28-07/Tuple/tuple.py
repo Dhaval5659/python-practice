293bc8843db88e2fd9c6d97d071a94c7b5aa4e15
Dhaval <dhavalmangukiya.work@gmail.com>
2026-07-28 12:46:49 +0530

    add tuple.py for practicing tuple updates and manipulations; remove unnecessary operators.py

---
+++ 28-07/Tuple/tuple.py
@@ -0,0 +1,21 @@
+# selection methods are same as list
+
+#Update Tuple 
+# to update we have to convert it in to list 
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+ 
+print(thistuple)
+
+y = ("orange",)
+thistuple += y
+print(thistuple)
